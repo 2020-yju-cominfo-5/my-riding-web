474505ef34a51978724102b3024e10864baf69e7
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-11-08 02:55:42 +0900

    [init, set-up] project init, add TitleBar

---
+++ src/components/navbar/TitleBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./TitleBar.css";
+
+function Title() {
+  return (
+    <div className="title-bar">
+      <div className="title-text">マイライディング</div>
+    </div>
+  );
+}
+
+export default Title;
