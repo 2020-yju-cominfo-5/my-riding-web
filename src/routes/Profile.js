79f06b286eebf2c83cc6eec69a6a014bc1c0280c
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-11-15 22:38:09 +0900

    [publishing] 프로필 페이지 구현

--- src/routes/Profile.js
+++ src/routes/Profile.js
@@ -12,12 +12,32 @@ function Profile() {
   return (
     <div className="profile">
       <div className="profileImg-section">
+        <div className="profileImg-title">PROFILE</div>
         <div className="img-wrapper">
           <div className="img"></div>
           <div className="upload-btn"></div>
         </div>
       </div>
-      <div className="profileInfo-section"></div>
+      <div className="profileInfo-section">
+        <ul className="profileInfo-wrapper">
+          <li>
+            <span className="profile-txt">아이디</span>
+            <span className="profile-id profileInfo">tyj01103</span>
+          </li>
+          <li>
+            <span className="profile-txt">비밀번호</span>
+            <button className="profileChange-btn">변경</button>
+          </li>
+          <li>
+            <span className="profile-txt">닉네임</span>
+            <span className="profile-nickname profileInfo">정재순</span>
+          </li>
+          <li>
+            <span className="profile-txt">가입일</span>
+            <span className="profile-date profileInfo">2020. 11. 02.</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
