5c5198e4dfe118c2277e3f3dcb7659221ee71e82
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-12-24 14:00:22 +0900

    [2020-12-24] 라이딩 경로 정보

--- src/components/Users/RidingRoute/RouteList.js
+++ src/components/Users/RidingRoute/RouteList.js
@@ -3,64 +3,7 @@ import { Link } from "react-router-dom";
 
 import "./RouteList.css";
 
-const RouteList = () => {
-  const routeList = [
-    {
-      id: 20,
-      date: "2020년 11월 01일",
-      name: "아양교 ~ 공항교 라이딩",
-      distance: "3.2km",
-      time: "00시간 00분",
-      like: "00",
-      count: "00",
-    },
-    {
-      id: 21,
-      date: "2020년 10월 29일",
-      name: "영진전문대학교 다운힐",
-      distance: "1.3km",
-      time: "00시간 00분",
-      like: "00",
-      count: "00",
-    },
-    {
-      id: 22,
-      date: "2020년 10월 27일",
-      name: "금호강 따라 라이딩",
-      distance: "6.7km",
-      time: "00시간 00분",
-      like: "00",
-      count: "00",
-    },
-    {
-      id: 23,
-      date: "2020년 10월 27일",
-      name: "강정보 라이딩",
-      distance: "13.2km",
-      time: "00시간 00분",
-      like: "00",
-      count: "00",
-    },
-    {
-      id: 24,
-      date: "2020년 10월 23일",
-      name: "경북대학교 ~ 산격동 라이딩",
-      distance: "3.2km",
-      time: "00시간 00분",
-      like: "00",
-      count: "00",
-    },
-    {
-      id: 25,
-      date: "2020년 10월 22일",
-      name: "라이딩은 즐겁다",
-      distance: "3.2km",
-      time: "00시간 00분",
-      like: "00",
-      count: "00",
-    },
-  ];
-
+const RouteList = ({ routeList }) => {
   return (
     <div className="route-list">
       <ul>
@@ -79,7 +22,7 @@ const RouteList = () => {
               <li key={id} className="value">
                 <div className="date">{date}</div>
                 <div className="name">
-                  <Link to={`route/${id}`}>{name}</Link>
+                  <Link to={`/route/${id}`}>{name}</Link>
                 </div>
                 <div className="distance">{distance}</div>
                 <div className="time">{time}</div>
@@ -88,7 +31,7 @@ const RouteList = () => {
                   <div className="count">{count} 회</div>
                 </div>
                 <div className="del-btn">
-                  <Link to={`route/delete/${id}`}>삭제</Link>
+                  <Link to={`/route/delete/${id}`}>삭제</Link>
                 </div>
               </li>
             );
