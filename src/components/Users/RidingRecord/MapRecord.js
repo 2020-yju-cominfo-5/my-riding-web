f8c4a448fa0ebdb9f6dc7e46198634ec13d1d921
JeongJaeSoon <wjdwotns1006@gmail.com>
2020-12-28 01:33:42 +0900

    [set-up] record chartjs

--- src/components/Users/RidingRecord/MapRecord.js
+++ src/components/Users/RidingRecord/MapRecord.js
@@ -5,11 +5,20 @@ import {
   Marker,
   Polyline,
 } from "@react-google-maps/api";
+import { Bar } from "react-chartjs-2";
 
 const MapRecord = () => {
-  const containerStyle = {
-    width: "100%",
-    height: "100%",
+  const [graph, setGraph] = useState();
+  const options = {
+    map: {
+      center: { lat: 35.89527, lng: 128.62256 },
+      disableDefaultUI: true,
+      zoom: 15,
+    },
+    polyline: {
+      strokeColor: "#32AF7B",
+      strokeWeight: 5,
+    },
   };
   const path = [
     { lat: 35.896725779882495, lng: 128.61992229254435 },
@@ -30,27 +39,104 @@ const MapRecord = () => {
     { lat: 35.89912484675867, lng: 128.6224709873701 },
     { lat: 35.89849574981631, lng: 128.6194481378103 },
   ];
-  const options = {
-    map: {
-      center: { lat: 35.89527, lng: 128.62256 },
-      disableDefaultUI: true,
-      zoom: 15,
-    },
-    polyline: {
-      strokeColor: "#32AF7B",
-      strokeWeight: 5,
-    },
-  };
 
+  const onLoad = useCallback(function callback(map) {
+    const elevator = new window.google.maps.ElevationService();
+
+    const getDistance = () => {
+      let distance = 0;
+
+      if (path.length < 2) {
+        return distance;
+      }
+
+      path.forEach((_, idx) => {
+        if (path.length - 1 == idx) {
+          return;
+        }
+
+        const point_1 = path[idx];
+        const point_2 = path[parseInt(idx) + 1];
+
+        const result = window.google.maps.geometry.spherical.computeDistanceBetween(
+          new window.google.maps.LatLng(point_1.lat, point_1.lng),
+          new window.google.maps.LatLng(point_2.lat, point_2.lng),
+        );
+
+        distance += Math.round(result);
+      });
+      return distance;
+    };
+
+    const plotElevation = (elevations, status) => {
+      if (status !== "OK") {
+        // Show the error code inside the chartDiv.
+        alert("실패");
+        return;
+      }
+
+      const dataSet = {
+        elevations: [],
+        distance: [],
+        locations: [],
+      };
+      elevations.map((element) => {
+        dataSet.elevations.push(element.elevation);
+        dataSet.locations.push(element.location.toJSON());
+        const tmpDistance = Math.round(
+          (getDistance() / 500) * dataSet.elevations.length,
+        );
+        const distanceTxt =
+          tmpDistance < 1000 ? tmpDistance + "m" : tmpDistance / 1000 + "km";
+        dataSet.distance.push(distanceTxt);
+      });
+
+      var data = {
+        labels: dataSet.distance,
+        datasets: [
+          {
+            label: "고도",
+            data: dataSet.elevations,
+            borderColor: "rgba(255, 99, 132, 1)",
+            backgroundColor: "rgba(255, 99, 132, 1)",
+            borderWidth: 2,
+            // fill: false,
+          },
+        ],
+      };
+
+      setGraph(<Bar data={data}></Bar>);
+    };
+
+    elevator.getElevationAlongPath(
+      {
+        path,
+        samples: 500,
+      },
+      plotElevation,
+    );
+  }, []);
+
+  const containerStyle = {
+    width: "100%",
+    height: "1%",
+  };
   return (
-    <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_KEY}>
-      <GoogleMap mapContainerStyle={containerStyle} options={options.map}>
-        {/* {path.map((item, idx) => {
+    <>
+      <LoadScript googleMapsApiKey={process.env.REACT_APP_GOOGLE_MAP_KEY}>
+        <GoogleMap
+          mapContainerStyle={containerStyle}
+          options={options.map}
+          onLoad={onLoad}
+        >
+          {/* {path.map((item, idx) => {
           return <Marker key={idx} position={item}></Marker>;
         })} */}
-        <Polyline path={path} options={options.polyline} />
-      </GoogleMap>
-    </LoadScript>
+          <Polyline path={path} options={options.polyline} />
+        </GoogleMap>
+      </LoadScript>
+      {graph}
+    </>
   );
 };
 
