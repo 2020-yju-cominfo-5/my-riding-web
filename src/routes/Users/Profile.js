beb4a26544cb4fcbb78bf9fa672d1e3aa2ac0193
JeongJaeSoon <wjdwotns1006@gmail.com>
2021-02-20 09:51:37 +0900

    [publishing] 프로파일 화면구성

--- src/routes/Users/Profile.js
+++ src/routes/Users/Profile.js
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from "react";
 import { getProfile } from "../../api/Auth";
+import { getDateContext } from "../../util";
+import "./Profile.css";
 
 const Profile = () => {
   const [data, setData] = useState({
@@ -8,6 +10,8 @@ const Profile = () => {
     user_picture: "",
     created_at: "",
   });
+  const [flag, setFlag] = useState(true);
+
   useEffect(() => {
     getProfile()
       .then((res) => {
@@ -19,13 +23,47 @@ const Profile = () => {
   }, []);
   const { user_account, user_nickname, user_picture, created_at } = data;
   return (
-    <>
-      프로파일 페이지 입니다.
-      {user_account}
-      {user_nickname}
-      {user_picture}
-      {created_at}
-    </>
+    <div className="profile">
+      <div className="top">
+        <div className="title">PROFILE</div>
+        <div className="img-section">
+          <div className="img"></div>
+          {flag && (
+            <div className="img-save">
+              <p className="img-save-btn">프로필 사진 저장</p>
+            </div>
+          )}
+          <div class="filebox">
+            <label for="btn-upload"></label>
+            <input type="file" id="btn-upload" />
+          </div>
+        </div>
+
+        {/* {user_picture} */}
+      </div>
+      <div className="bottom">
+        <ul>
+          <li>
+            <span className="title">아이디</span>
+            <span className="value">{user_account}</span>
+          </li>
+          <li>
+            <span className="title">비밀번호</span>
+            <span className="value">변경</span>
+          </li>
+          <li>
+            <span className="title">닉네임</span>
+            <span className="value">{user_nickname}</span>
+          </li>
+          <li>
+            <span className="title">가입일</span>
+            <span className="value">
+              {getDateContext({ date: created_at })}
+            </span>
+          </li>
+        </ul>
+      </div>
+    </div>
   );
 };
 
