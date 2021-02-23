79aad2da2f56eff9da8ee8ad8b7197ce39b001ac
JeongJaeSoon <wjdwotns1006@gmail.com>
2021-02-24 01:20:54 +0900

    [modify] 라이딩 일지 강조 효과 추가

--- src/components/Users/RidingRecord/items/Main/RecordMainChartContens.js
+++ src/components/Users/RidingRecord/items/Main/RecordMainChartContens.js
@@ -1,11 +1,11 @@
 import React from "react";
 import WeekStat from "../../items/WeekStat";
 
-const RecordMainChartContens = ({ year, stats }) => {
+const RecordMainChartContens = ({ year, stats, label }) => {
   return (
     <ul className="chart-contents">
       {stats.map((stat, idx) => {
-        return <WeekStat key={idx} stat={stat} year={year} />;
+        return <WeekStat key={idx} stat={stat} year={year} label={label} />;
       })}
     </ul>
   );
