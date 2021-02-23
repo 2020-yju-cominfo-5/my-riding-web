83c7749a0d9bd822383d3bc6db19b54852f59dde
JeongJaeSoon <wjdwotns1006@gmail.com>
2021-02-24 00:45:07 +0900

    [modify] record 기록 없을 경우 예외 처리

--- src/components/Users/RidingRoute/items/Show/RouterShowInfoDetail.js
+++ src/components/Users/RidingRoute/items/Show/RouterShowInfoDetail.js
@@ -11,6 +11,8 @@ const RouterShowInfoDetail = ({ data }) => {
     topRankerRecord,
   } = data;
 
+  const isMyRecordExist = Number.isInteger(myRank);
+  
   return (
     <div className="bottom">
       <div className="left">
@@ -21,44 +23,64 @@ const RouterShowInfoDetail = ({ data }) => {
         <div className="my-record">
           <div className="record-title">
             <p className="txt record-sub-title">내 라이딩 기록</p>
-            <p className="my-rank record-sub-title">
-              {myRank} / {usrCount}
-            </p>
+            {isMyRecordExist && (
+              <p className="my-rank record-sub-title">
+                {myRank} / {usrCount}
+              </p>
+            )}
           </div>
           <div className="record-value">
-            <div className="img"></div>
-            <div className="record">
-              <div className="wrapper">
-                <p className="title">통산 최고 기록</p>
-                <p className="value">
-                  {getTimeContext({ time: myBestRecord })}
-                </p>
-              </div>
-              <div className="wrapper">
-                <p className="title">평균 기록</p>
-                <p className="value">{getTimeContext({ time: myAvgRecord })}</p>
-              </div>
-            </div>
+            {isMyRecordExist ? (
+              <>
+                <div className="img"></div>
+                <div className="record">
+                  <div className="wrapper">
+                    <p className="title">통산 최고 기록</p>
+                    <p className="value">
+                      {getTimeContext({ time: myBestRecord })}
+                    </p>
+                  </div>
+                  <div className="wrapper">
+                    <p className="title">평균 기록</p>
+                    <p className="value">
+                      {getTimeContext({ time: myAvgRecord })}
+                    </p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className="no-data">{myRank}</div>
+            )}
           </div>
         </div>
+        {/* ) : (
+          <div className="my-record">{myRank}</div>
+        )} */}
+
         <div className="best-record">
           <div className="record-title">
             <p className="record-sub-title">가장 빠른 기록</p>
           </div>
           <div className="record-value">
-            <div className="img"></div>
-            <div className="record">
-              <div className="user-name">
-                <i className="fas fa-crown"></i>
-                <p className="txt">{topRankerAccount}</p>
-              </div>
-              <div className="wrapper">
-                <p className="title">최고 기록</p>
-                <p className="value">
-                  {getTimeContext({ time: topRankerRecord })}
-                </p>
-              </div>
-            </div>
+            {isMyRecordExist ? (
+              <>
+                <div className="img"></div>
+                <div className="record">
+                  <div className="user-name">
+                    <i className="fas fa-crown"></i>
+                    <p className="txt">{topRankerAccount}</p>
+                  </div>
+                  <div className="wrapper">
+                    <p className="title">최고 기록</p>
+                    <p className="value">
+                      {getTimeContext({ time: topRankerRecord })}
+                    </p>
+                  </div>
+                </div>
+              </>
+            ) : (
+              <div className="no-data">{myRank}</div>
+            )}
           </div>
         </div>
       </div>
